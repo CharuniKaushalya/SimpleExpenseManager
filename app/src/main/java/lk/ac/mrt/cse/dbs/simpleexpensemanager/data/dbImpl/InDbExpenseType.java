7d86953e0e2160568f443ed8aa9bf625932a5def
CharuniKaushalya <snkaushi@gmail.com>
2015-12-05 21:26:37 +0530

    create tansaction and expense_Type table

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/dbImpl/InDbExpenseType.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/dbImpl/InDbExpenseType.java
@@ -4,28 +4,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
-
 /**
  * Created by Charuni on 12/5/2015.
  */
-public class InDbAccountDAO extends SQLiteOpenHelper {
+public class InDbExpenseType extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "130274U.db";
-    public static final String TABLE_NAME = "account";
-    public static final String COL_1 = "accountNo";
-    public static final String COL_2 = "bankName";
-    public static final String COL_3 = "accountHolderName";
-    public static final String COL_4 = "balance";
-    public InDbAccountDAO(Context context) {
+    public static final String TABLE_NAME = "expense_type";
+
+    public InDbExpenseType(Context context) {
         super(context,DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +" (accountNo VARCHAR(45) PRIMARY KEY NOT NULL,bank TEXT,accountHolderName TEXT,balance DOUBLE)");
-
+        db.execSQL("create table " + TABLE_NAME +" (Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,name TEXT)");
     }
 
     @Override
--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/dbImpl/InDbTransaction.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/dbImpl/InDbTransaction.java
@@ -4,28 +4,23 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
-
 /**
  * Created by Charuni on 12/5/2015.
  */
-public class InDbAccountDAO extends SQLiteOpenHelper {
+
+public class InDbTransaction extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "130274U.db";
-    public static final String TABLE_NAME = "account";
-    public static final String COL_1 = "accountNo";
-    public static final String COL_2 = "bankName";
-    public static final String COL_3 = "accountHolderName";
-    public static final String COL_4 = "balance";
-    public InDbAccountDAO(Context context) {
+    public static final String TABLE_NAME = "transaction";
+
+    public InDbTransaction(Context context) {
         super(context,DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +" (accountNo VARCHAR(45) PRIMARY KEY NOT NULL,bank TEXT,accountHolderName TEXT,balance DOUBLE)");
-
+        db.execSQL("create table " + TABLE_NAME +" (transactionID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,amount DOUBLE,date DATE,FOREIGN KEY (accountNo) REFERENCES account(accountNo),FOREIGN KEY (expense_type_id) REFERENCES expense_type(Id))");
     }
 
     @Override
